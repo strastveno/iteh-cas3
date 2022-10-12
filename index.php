d28b4d31423c7e0ba2ddc477fbb788d51d0b678d
Petar Lukovac <petar.lukovac@fon.bg.ac.rs>
2022-10-12 22:31:06 +0200

    Izmenjen tekst login forme

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>FON: Zakazivanje kolokvijuma</title>
 
 </head>
+
 <body>
     <div class="login-form">
         <div class="main-div">
             <form method="POST" action="#">
                 <div class="container">
-                    <label class="username">Korisnicko ime</label>
-                    <input type="text" name="username" class="form-control"  required>
+                    <label class="username">Korisnick</label>
+                    <input type="text" name="username" class="form-control" required>
                     <br>
                     <label for="password">Lozinka</label>
                     <input type="password" name="password" class="form-control" required>
@@ -22,7 +24,8 @@
             </form>
         </div>
 
-        
+
     </div>
 </body>
+
 </html>
